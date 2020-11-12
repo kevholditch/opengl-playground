3974de4241bd26cb26d276baec3464b22cd0d2fb
Kevin Holditch <kevholditch@hotmail.com>
2020-11-12 20:29:10 +0000

    saving

---
+++ triangle1/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
+	"log"
+	"runtime"
+)
+
+const width, height = 800, 600
+
+func init() {
+	// GLFW event handling must run on the main OS thread
+	runtime.LockOSThread()
+}
+
+func main()  {
+	if err := glfw.Init(); err != nil {
+		log.Fatalln("failed to initialize glfw:", err)
+	}
+	defer glfw.Terminate()
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 2)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	window, err := glfw.CreateWindow(width, height, "Kevin - Demo", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	for !window.ShouldClose() {
+
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+		gl.Begin(gl.TRIANGLES)
+		gl.Vertex2f(-0.5, -0.5)
+		gl.Vertex2f(0.0, 0.5)
+		gl.Vertex2f(0.5, -0.5)
+		gl.End()
+
+
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
--- triangle2/main.go
+++ triangle2/main.go
@@ -23,7 +23,7 @@ func main()  {
 	glfw.WindowHint(glfw.Resizable, glfw.False)
 	glfw.WindowHint(glfw.ContextVersionMajor, 2)
 	glfw.WindowHint(glfw.ContextVersionMinor, 1)
-	window, err := glfw.CreateWindow(width, height, "Cube", nil, nil)
+	window, err := glfw.CreateWindow(width, height, "Kevin - Demo", nil, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -31,5 +31,25 @@ func main()  {
 
 	if err := gl.Init(); err != nil {
 		panic(err)
+	}
+
+	positions := []float32{
+		-0.5, -0.5,
+		0.0, 0.5,
+		0.5, -0.5,
+	}
+
+	var buffer uint32
+	gl.GenBuffers(1, &buffer)
+	gl.BindBuffer(gl.ARRAY_BUFFER, buffer)
+	gl.BufferData(gl.ARRAY_BUFFER, len(positions) * 4, gl.Ptr(positions), gl.STATIC_DRAW)
+
+	for !window.ShouldClose() {
+
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+		gl.DrawArrays(gl.TRIANGLES, 0, 3)
+
+		window.SwapBuffers()
+		glfw.PollEvents()
 	}
 }
